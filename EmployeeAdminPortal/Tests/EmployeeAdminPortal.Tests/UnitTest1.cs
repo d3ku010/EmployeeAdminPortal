d9f646ff183560fbfcddabde8f4f4d8abff5b3aa
d3ku010 <firdousfiru0@gmail.com>
2025-01-24 09:28:31 +0000

    Add test project and sample test

---
+++ EmployeeAdminPortal/Tests/EmployeeAdminPortal.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EmployeeAdminPortal.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ EmployeeAdminPortal/Tests/EmployeeAdminPortalTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace EmployeeAdminPortal.Tests
+{
+    public class EmployeeAdminPortalTests
+    {
+        [Fact]
+        public void SampleTest()
+        {
+            Assert.True(true); // Simple test to ensure the pipeline works
+        }
+    }
+}
