3b63242ac1355f7454b303f362bcf429f431ebc3
Ameenul Firdous <127436359+d3ku010@users.noreply.github.com>
2025-02-03 18:31:39 +0530

    Add NLog

---
+++ EmployeeAdminPortal/Logs/LoggingService.cs
@@ -0,0 +1,30 @@
+﻿using NLog;
+namespace EmployeeAdminPortal.Logs
+{
+    public class LoggingService : ILoggingService
+    {
+        private static readonly NLog.ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void LogError(string message, Exception? ex = null)
+        {
+            logger.Error(ex, message);
+        }
+
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            logger.Warn(message);
+        }
+    }
+
+}
+
