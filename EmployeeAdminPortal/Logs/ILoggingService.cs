1402304cd2a37bb713ac60a79ae2d04710240b7d
Ameenul Firdous <127436359+d3ku010@users.noreply.github.com>
2025-02-03 16:15:56 +0530

    NLog added

---
+++ EmployeeAdminPortal/Logs/ILoggingService.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeAdminPortal.Logs
+{
+    public interface ILoggingService
+    {
+        void LogInfo(string message);
+        void LogError(string message, Exception? ex = null);
+        void LogDebug(string message);
+        void LogWarning(string message);
+    }
+}
